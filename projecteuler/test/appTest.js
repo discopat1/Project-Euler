f605cd322e6b59d57d461d1c8368b221eb0004f9
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2019-02-09 16:41:41 -0700

    set up and started on number 1

---
+++ projecteuler/test/appTest.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
+const findMultiples = require('../exercise1').findMultiples;
+
+
+describe('App', function(){
+    // it('sayHello should return hello', function(){
+    //     assert.equal(sayHello(), 'hello')
+    // });
+
+    // it('sayHello should return type string', function() {
+    //     let result = sayHello();
+    //     assert.typeOf(result, 'string');
+    // });
+
+    // it('addNumbers should be above 5', function() {
+    //     let result = addNumbers(5,5);
+    //     assert.isAbove(result, 5);
+    // });
+
+    // it('addNumbers should return type number', function() {
+    //     let result = addNumbers(5,5);
+    //     assert.typeOf(result, 'number');
+    // });
+    it('findMultiples should return number which divided by 3 or 5 have remainder of 0', function() {
+        let result = findMultiples(20);
+        assert.equal(result, true);
+    });
+});
